81114bc6d44b58c6785898856a6e5d9caaaf7c99
ChineseThunder <simo09j9@stud.kea.dk>
2023-02-01 09:20:35 +0100

    Initial Commit

---
+++ Business-cardapp/Business_cardappApp.swift
@@ -0,0 +1,17 @@
+//
+//  Business_cardappApp.swift
+//  Business-cardapp
+//
+//  Created by Simon Igild on 01/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Business_cardappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
