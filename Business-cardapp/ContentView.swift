fb32755eee53769b4cd4cccb5e54b0c97010068f
ChineseThunder <simo09j9@stud.kea.dk>
2023-02-01 11:14:49 +0100

    done

--- Business-cardapp/ContentView.swift
+++ Business-cardapp/ContentView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 17.0) {
+            Image("EG")
+                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                .padding(.top, -100.0)
+                .imageScale(.small)
+            Image("Image")
+                .resizable()
+                .padding(.top, -120.0)
+                .padding(.bottom, +80)
+                .frame(width: 406.0, height: 500.0)
+                
+                
+            Text("Simon Igild")
+                .padding(.top, -60.0)
+            Text("Elgiganten Frederiksberg")
+                .padding(.top, -45.0)
+            Text("Trusted Advisor")
+                .padding(.top, -25.0)
+            Text("+ 45 30 20 84 30")
+                .padding(.top, -7.0)
+            
         }
-        .padding()
     }
 }
 
